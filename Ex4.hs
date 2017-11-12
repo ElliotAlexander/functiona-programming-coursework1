8eec3da7753f5569ffe22dfd0a4f3248ef706a2d
Elliot Alexander <epurvis97@gmail.com>
2017-11-12 23:58:16 +0000

    Worked on Q4, added a tidy check for duplicates inside a list

--- Ex4.hs
+++ Ex4.hs
@@ -5,24 +5,10 @@ data TypeOfSort = Ascending | NonDescending | Constant | NonAscending | Descendi
 sortType :: Ord a => [a] -> TypeOfSort
 sortType xs
   | xs == [] = Ascending
-  | check_identical xs = Constant
-  -- Stricly increase
-  | sort xs == xs && check_duplicates (sort xs) == False = Ascending
-  -- Strictly decrease
-  | reverse (sort xs) == xs && check_duplicates (sort xs) == False = Descending
-  -- Increase or stay the same
-  | sort xs == xs = NonDescending
-  -- Decrease or stay the same
-  | reverse (sort xs) == xs = NonAscending
-  | otherwise = NotSorted
+  | length no_dup == length xs = Ascending
+  where 
+    no_dup = map head (group(sort xs))
 
-
-check_identical :: Ord a => [a] -> Bool
-check_duplicates (x:xs)
-  | xs == [] = True
-  | x == head xs = check_duplicates xs
-  | otherwise = False
-
-check_duplicates :: Ord a => [a] -> Bool 
-check_identical (x:xs)
-  | xs == [] = true
+    -- True it no dups
+no_dup_check :: Ord a => [a] -> Bool
+no_dup_check xs = length (map head (group(sort xs))) == length xs
