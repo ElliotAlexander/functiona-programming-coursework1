c9aa6e95bbaf6e48299483dbcf5c6becdb71b23c
Elliot Alexander <epurvis97@gmail.com>
2017-11-13 18:14:51 +0000

    Seemingly working ex8...

--- Ex8.hs
+++ Ex8.hs
@@ -1,6 +1,22 @@
 import Data.List
 
+-- SEEMINGLY WORKING
+-- NEEDS MORE TESTING
+
 newtonRootSequence :: Double -> [Double]
-newtonRootSequence d 
-    |                                                                           
+newtonRootSequence d = newtonRootSequence' d 1.0
+
+
+newtonRootSequence' :: Double -> Double -> [Double]
+newtonRootSequence' d xn = [((xn+d/xn)/2)]  ++ newtonRootSequence' d ((xn + d / xn) /2)
+
+newtonRoot :: Double -> Double -> Double
+newtonRoot d epsilon = newtonRoot' d epsilon 1
+
+
+newtonRoot' :: Double -> Double -> Double -> Double
+newtonRoot' d epsilon xn
+    | abs (xn - xnPlusOne) > epsilon = newtonRoot' d epsilon xnPlusOne
+    | otherwise = xn
     where
+        xnPlusOne = ((xn + d) / xn)/2
