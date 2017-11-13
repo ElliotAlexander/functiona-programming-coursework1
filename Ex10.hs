9406e0cced85a4fd9cafad7c35c084f396502dfe
ElliotAlexander <epurvis97@gmail.com>
2017-11-13 22:35:39 +0000

    Ex10 is giving me a painful disease

--- Ex10.hs
+++ Ex10.hs
@@ -2,7 +2,19 @@ import Data.Char
 
 
 encode :: String -> [Int]
-encode s 
-    | s == [] = []
-    | otherwise = [ fromEnum x | x <- s ]
+encode xs 
+    | xs == [] = []
+    | otherwise =  y
+    where 
+        y = [ fromEnum x | x <- xs]
 
+tobinary :: Int -> [Int]
+tobinary 0 = [0]
+tobinary x  
+    | x `mod` 2 == 1 = 1 : tobinary (x `div` 2)
+    | x `mod` 2 == 0 = 0 : tobinary (x `div` 2)
+
+output :: [Int] -> [Int]
+output (x:xs)
+    | (x:xs) == [] = []
+    | otherwise = (reverse (tobinary x)) ++ output xs
