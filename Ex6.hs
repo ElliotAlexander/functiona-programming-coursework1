312a17029531ba56166c6d9515fafa14e0d2c204
ElliotAlexander <epurvis97@gmail.com>
2017-11-14 21:35:10 +0000

    Marked ex6 as complete

--- Ex6.hs
+++ Ex6.hs
@@ -1,5 +1,7 @@
 import Data.List
 
+-- This seems to work wonderfully
+
 data DistancePoint = DistancePoint {distance :: Float, coord :: (Float, Float)} deriving (Eq, Ord, Show)
 neighbours :: (Floating a, Ord a) => Int -> (a,a) -> [(a,a)] -> [(a,a)]
 neighbours k (p,q) xs = take k [x | (_,x) <- sort([ (sqrt((p-r)^2 + (q-s)^2), (r,s)) | (r,s) <- xs])]
