ac7b420a18913b14d1c869999f91b09259466f84
ElliotAlexander <epurvis97@gmail.com>
2017-11-14 16:41:31 +0000

    Ex11 done in sensual fashion

---
+++ Ex11.hs
@@ -0,0 +1,10 @@
+import Data.Char
+import Data.List
+
+decode :: [Int] -> String
+decode xs
+    | xs == [] = []
+    | otherwise = [chr (toDec (take 8 xs))] ++ decode (drop 9 xs)
+
+toDec :: [Int] -> Int
+toDec = foldl' (\acc x -> acc * 2 + x) 0   
--- Ex5.hs
+++ Ex5.hs
@@ -3,7 +3,7 @@ import Data.Char
 rpcalc :: String -> [Char]
 rpcalc (x:xs) 
     | xs==[]=[]
-    | otherwise = foldr (head symbols) (head ints) (ints !! 1) $$ ints : 
+    | otherwise = rpcalc xs  
     where
         ints = [ x | x <- xs, isDigit x]
         symbols = [ x | x <- xs, isDigit x == False]
