ad5d1560a23b7bb43e9d5b5c75fdae5f7d87f3d9
ElliotAlexander <epurvis97@gmail.com>
2017-11-14 15:47:04 +0000

    Ex10 is working

--- Ex10.hs
+++ Ex10.hs
@@ -1,20 +1,33 @@
 import Data.Char
 
+-- I think this is working....
 
 encode :: String -> [Int]
 encode xs 
     | xs == [] = []
-    | otherwise =  y
+    | otherwise =  output y
     where 
         y = [ fromEnum x | x <- xs]
 
+
 tobinary :: Int -> [Int]
-tobinary 0 = [0]
 tobinary x  
+    | x == 0 = []
     | x `mod` 2 == 1 = 1 : tobinary (x `div` 2)
     | x `mod` 2 == 0 = 0 : tobinary (x `div` 2)
 
 output :: [Int] -> [Int]
 output (x:xs)
-    | (x:xs) == [] = []
-    | otherwise = (reverse (tobinary x)) ++ output xs
+    | x == 0 = []
+    | xs == [] = y
+    | otherwise = y ++ [1] ++ output xs
+    where
+        y = bitwiseor (reverse(tobinary x))
+
+bitwiseor :: [Int] -> [Int]
+bitwiseor xs
+    | length xs == 8 = xs
+    | otherwise = addstring ++ xs
+    where
+        ms = 8 - length xs
+        addstring = take ms (repeat 0)
