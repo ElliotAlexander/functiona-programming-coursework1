4c96eaf0bb47a280302b7baa9534f682f18b9ef2
ElliotAlexander <epurvis97@gmail.com>
2017-11-15 11:45:56 +0000

    Somewhat working hyperoperators

--- Ex9.hs
+++ Ex9.hs
@@ -1,1 +1,13 @@
 -- Attempted
+
+hyperOperator :: Int -> Int -> Int -> Int
+hyperOperator n a b 
+    | n == 0 = b + 1
+    | b == 0 = hyperOperator' n a
+    | otherwise = hyperOperator (n-1) a (hyperOperator n a (b-1))
+
+hyperOperator' :: Int -> Int -> Int
+hyperOperator' n a
+    | n == 1 = a
+    | n == 2 = 0
+    | n >= 3 = 1
